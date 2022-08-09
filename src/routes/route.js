615eec5cf36ed8af369a32b28b5dcfbb0515e74f
ujjwaltyai82 <ujjwalroger@gmail.com>
2022-08-09 21:34:33 +0530

    commit this api file

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,7 +70,50 @@ router.get("/films", function(req, res){
        }]
        //send all the films
       res.send(films) 
-})
+});
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    //  let missingNumber
+    function missnum(a, b) {
+  
+        let total = Math.floor((b + 1) * (b + 2) / 2);
+        for (let i = 0; i < b; i++)
+            total -= a[i];
+            return total;
+    }
+    let b = arr.length;
+    let missingNumber = missnum(arr, b);
+   
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+});
+
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len =arr.length
+
+    let total = 0;
+
+    for (var i in arr){
+     total += arr[i];
+
+    }
+
+let first =arr [0]
+let last = arr.pop()
+let  sum = (len + 1) * (first+last)/2
+
+let missingNumber =  sum - total
+   
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+
+});
+
+
+
 
 router.get("/films/:filmId", function(req, res){
     const films = [ {
@@ -104,4 +147,5 @@ router.get("/films/:filmId", function(req, res){
 })
 
 module.exports = router;
+
 // adding this comment for no reason
