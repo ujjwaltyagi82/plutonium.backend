25712df6cff767a1b78912135ee6e8489593e019
ujjwaltyai82 <ujjwalroger@gmail.com>
2022-08-10 19:43:43 +0530

    post array

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,64 @@ const underscore = require('underscore')
 
 const router = express.Router();
 
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+
+
+    let newperson = req.body
+    let newpersoname = newperson.name
+    let Repeated = false
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newpersoname) {
+            Repeated = true;
+            break;
+        }
+    }
+    if (Repeated) {
+        res.send("Please check your name this name is already exits")
+    } else {
+        players.push(newperson)
+        res.send(players)
+    }
+});
+
+ 
+
+
+
+
 router.get('/test-me', function (req, res) {
     myHelper.printDate()
     myHelper.getCurrentMonth()
